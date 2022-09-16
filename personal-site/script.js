0775b89f231143b25f6af11126e2bfda40e7fa52
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-09-16 15:55:13 -0400

    worked on the mobile navbar. still need js close/menu

--- personal-site/script.js
+++ personal-site/script.js
@@ -1,1 +1,24 @@
-document.getElementById("close-button")
+// write code in DOMContentLoaded to load page properly before executing js.
+document.addEventListener("DOMContentLoaded", function(event) {
+    let closeButton = document.getElementById("close-button");
+
+closeButton.onclick = function() {
+    // {alert('bleh');};
+    let mobileNavbar = document.getElementsByClassName("mobile-navbar");
+
+    
+    
+    if (mobileNavbar[0].style.display === "none") {
+        mobileNavbar[0].style.display = "block";
+        closeButton.style.display = "block"; 
+    }
+    else {
+        mobileNavbar[0].style.display = "none";
+        closeButton.style.display = "none"; 
+    }
+
+    // let closeButton = document.getElementById("close-button");
+    // document.getElementsByClassName("mobile-navbar").style.display = "none";
+    }
+});
+
