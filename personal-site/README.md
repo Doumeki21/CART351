b52a80f83d36268af600299533b1a8e1bb9d4391
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-10-04 02:07:44 -0400

    added cursor and started proposal page

--- personal-site/README.md
+++ personal-site/README.md
@@ -7,6 +7,9 @@
 **References**
 - [W3schools - CSS Horizontal Navigation Bar](https://www.w3schools.com/css/css_navbar_horizontal.asp)
 - [Website layout inspiration](https://resilientwebdesign.com/)
+- [Link and button hover effects](https://www.youtube.com/watch?v=ceNMP-aQkQ4)
+- [Cursor effect](https://www.youtube.com/watch?v=TpwpAYi-p2w)
+
 - [Toggle class in vanilla js](https://ryfarlane.com/article/toggle-class-vanilla-javascript)
 - [Creating and animating a hamburger icon to X](https://ryfarlane.com/article/animated-hamburger-menu-icon-to-x-close-icon-css)
 - [Hamburger transition to X](https://www.w3schools.com/howto/howto_css_menu_icon.asp)
--- personal-site/js/cursor.js
+++ personal-site/js/cursor.js
@@ -1,20 +1,42 @@
-//put on the parent;
-document.getElementById("parent").addEventListener("mousemove", mouseCapture);
+// //put on the parent;
+// document.getElementById("parent").addEventListener("mousemove", mouseCapture);
 
-function mouseCapture() {
+// function mouseCapture() {
 
-  //2:: put in center
-  let mBox = document.getElementById("moveMe").getBoundingClientRect();
-  // console.log(mBox);
+//   //2:: put in center
+//   let mBox = document.getElementById("moveMe").getBoundingClientRect();
+//   // console.log(mBox);
 
-  // //3:: need the parent offset::
-  //     //NOTE that mouseEVent coordinates are ALWAYS relative to the window ...
-  //getBoundingClientRect becomes parent here!
-  let pBox = this.getBoundingClientRect();
-  //Here we calculate the offset depending where the parent is!!
-  let mouse_offset_x = event.clientX - pBox.x;
-  let mouse_offset_y = event.clientY - pBox.y;
+//   // //3:: need the parent offset::
+//   //     //NOTE that mouseEVent coordinates are ALWAYS relative to the window ...
+//   //getBoundingClientRect becomes parent here!
+//   let pBox = this.getBoundingClientRect();
+//   //Here we calculate the offset depending where the parent is!!
+//   let mouse_offset_x = event.clientX - pBox.x;
+//   let mouse_offset_y = event.clientY - pBox.y;
 
-  document.getElementById("moveMe").style.left = (mouse_offset_x - mBox.width / 2) + 1 + "px";
-  document.getElementById("moveMe").style.top = (mouse_offset_y - mBox.height / 2) + 1 + "px";
+//   document.getElementById("moveMe").style.left = (mouse_offset_x - mBox.width / 2) + 1 + "px";
+//   document.getElementById("moveMe").style.top = (mouse_offset_y - mBox.height / 2) + 1 + "px";
+// }
+
+let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll(".fancy-link li");
+
+window.addEventListener("mousemove", cursor);
+
+function cursor(e) {
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
 }
+
+// create array to track when the mouse is hovering on any link
+navLinks.forEach(link => {
+  link.addEventListener("mouseleave", () => {
+    // this class list is created and will be reffered in the css.
+    mouseCursor.classList.remove("link-grow");
+  });
+  link.addEventListener("mouseover", () => {
+    // this class list is created and will be reffered in the css.
+    mouseCursor.classList.add("link-grow");
+  });
+});
