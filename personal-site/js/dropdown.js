df3ec2cf2d71128df8a2106c1c5453dd7e29a622
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-10-05 20:23:44 -0400

    fixed navbar + btn z-index and position fx

--- personal-site/js/dropdown.js
+++ personal-site/js/dropdown.js
@@ -1,37 +1,42 @@
-$(document).ready(function(){
-    $("#button").on("mouseover", function(){
-        // console.log("hover");
-        $("#dropdown-container").show();
-    })
-    $("#button").on("mouseout", function(){
-        // console.log("hover");
-        $("#dropdown-container").hide();
-    })
+$(document).ready(function () {
+  // 1
+  // open the dropdown menu when hovering the proposal button.
+  $("#button").on("mouseover", function () {
+    $("#dropdown-container").show();
+  });
+  // hide when mouse leaves
+  $("#button").on("mouseout", function () {
+    $("#dropdown-container").hide();
+  });
 
-    $("#dropdown-container").on("mouseover", function(){
-        console.log("hover");
-        $("#dropdown-container").show();
-    })
-    $("#dropdown-container").on("mouseout", function(){
-        console.log("out");
-        $("#dropdown-container").hide();
-    })
+  // 2
+  // keep the dropdown contents visible while the mouse is hovering overing it.
+  $("#dropdown-container").on("mouseover", function () {
+    console.log("hover");
+    $("#dropdown-container").show();
+  });
+  // hide the dropdown contents when mouse leaves the area.
+  $("#dropdown-container").on("mouseout", function () {
+    console.log("out");
+    $("#dropdown-container").hide();
+  });
 
-    $("#similar-project").on("mouseover", function(){
-        // console.log("hover");
-        $("#sub-dropdown-container").show();
-    })
-    $("#similar-project").on("mouseout", function(){
-        // console.log("hover");
-        $("#sub-dropdown-container").hide();
-    })
+  // 3
+  // show the sub category contents when the cursor hovers over the similar projects anchor tag.
+  $("#similar-project").on("mouseover", function () {
+    $("#sub-dropdown-container").show();
+  });
+  // hide the contents when mouse leaves
+  $("#similar-project").on("mouseout", function () {
+    $("#sub-dropdown-container").hide();
+  });
 
-    $("#sub-dropdown-container").on("mouseover", function(){
-        // console.log("hover");
-        $("#sub-dropdown-container").show();
-    })
-    $("#sub-dropdown-container").on("mouseout", function(){
-        // console.log("hover");
-        $("#sub-dropdown-container").hide();
-    })
+  // 4
+  //  keep the sub categories visible for as long as the mouse is hovered on top of it .
+  $("#sub-dropdown-container").on("mouseover", function () {
+    $("#sub-dropdown-container").show();
+  });
+  $("#sub-dropdown-container").on("mouseout", function () {
+    $("#sub-dropdown-container").hide();
+  });
 });
