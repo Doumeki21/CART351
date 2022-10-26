2ae3663c3570f567ae361138f1899ab3ed193db4
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-10-26 15:30:50 -0400

    ex2: shows second line of text, need to fix the animation

--- exercises/exercise-2/js/main.js
+++ exercises/exercise-2/js/main.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
+  let transtate = "state-two";
+  
+
   $("#choose-day").submit((e) => {
-    let dayValue = $("#week-day").val();
     e.preventDefault(); //prevents the form from returning to its initial state after the page reloads from triggering the submit btn.
+    let dayValue = $("#week-day").val();
+    if (dayValue === "Friday") {
+      transtate = "state-one";
+    }
+    if (dayValue === "Saturday") {
+      transtate = "state-three";
+    }
+    if (dayValue === "Sunday") {
+      transtate = "state-four";
+    }
+    if (dayValue === "Monday") {
+      transtate = "state-five";
+    }
+    console.log(transtate);
+
+    
 
     $(`#results-container`).empty(); //resets the results.
 
@@ -10,6 +28,7 @@ $(document).ready(function () {
       // set boolean to true
       loaded = true;
       findDayData(data, dayValue);
+
     })
       // fail
       .fail(function () {
@@ -20,30 +39,42 @@ $(document).ready(function () {
   //   Dsiplay the text results on screen.
   function displayDay(day, dayQuality, sleepQuality) {
     let textDay = $(
-      `<h2>On ${day}, I had a ${dayQuality} day.</h2> <br> <h2>I got ${sleepQuality} hours of sleep.`
+      `<h2>On ${day}, I had a ${dayQuality} day.</h2> <h2>I got ${sleepQuality} hours of sleep.</h2>`
     );
     textDay.addClass("text-day").appendTo("#results-container");
   }
+
+  // Does what the filter function
 
   function findDayData(data, dayValue) {
     for (let i = 0; i < data.days.length; i++) {
       $.each(data.days[i], function (index, value) {
         if (value === dayValue) {
-          //   hourToColor(data.days[i].Awake, data.days[i].Asleep);
-          //   wakeUpNight(data.days[i].WakeUp);
-          //   bedAndUpTime(data.days[i].Bed, data.days[i].Up);
-          displayDay(data.days[i].day, data.days[i].quality);
+          displayDay(data.days[i].day, data.days[i].quality,data.days[i].sleep);
         }
       });
     }
   }
+
+  // // JQUERY ANIMATE
+  // $( "#submit-button" ).click(function() {
+  //   $( "#pulse" ).animate({
+  //     opacity: 0.5,
+  //     transform: "toggle"
+  //   }, 5000, function() {
+  //     // Animation complete.
+  //   });
+  // });
 
   // https://www.kirupa.com/html5/looping_a_css_transition.htm
   //   1. TRIGGER THE PULSE ANIMATION
   let theCircle = document.querySelector("#pulse");
 
   function setup() {
-    theCircle.addEventListener("mouseover", setInitialClass, false);
+  //   console.log( document.getElementById("submit-button"));
+  //  document.getElementById("submit-button").addEventListener("click", setInitialClass, false);
+   theCircle.addEventListener("mouseover", setInitialClass, false);
+
 
     theCircle.addEventListener("transitionend", loopTransition, false);
     theCircle.addEventListener("webkitTransitionEnd", loopTransition, false);
@@ -54,19 +85,19 @@ $(document).ready(function () {
   setup();
 
   function setInitialClass(e) {
-    theCircle.className = "state-two";
+    theCircle.className = transtate;
+    console.log(theCircle.className);
   }
 
   function loopTransition(e) {
-    // alternate b/w classes 
+    // alternate b/w classes
+    console.log(transtate);
     if (e.propertyName == "opacity") {
-        if (theCircle.className == "state-two") {
-            theCircle.className = "state-one";
-        } else {
-            theCircle.className = "state-two";
-        }
+      if (theCircle.className == transtate) {
+        theCircle.className = "state-one";
+      } else {
+        theCircle.className = transtate;
+      }
     }
-}
-
-
+  }
 });
