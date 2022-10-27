5086249a5ca07e6fe9f6deff39d92c0aab320e76
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-10-26 21:41:01 -0400

    ex 2- animation solved

---
+++ exercises/exercise-2 - v2/js/main.js
@@ -0,0 +1,140 @@
+$(document).ready(function () {
+  let transtate = "state-two";
+  // let circleState = "state-two";
+  let scaleVal = 1;
+  let speed = 0.01;
+  let circleScale = 1;
+
+  window.requestAnimationFrame(loop);
+
+    function loop() {
+      if (scaleVal < 0.5 || scaleVal > circleScale) {
+        speed *= -1;
+      }
+      console.log(scaleVal);
+      // document.getElementById("content").style.width = `${width}px`;
+      // document.getElementById("content").style.height = `${width}px`;
+      document.getElementById("pulse").style.transform = `scale(${scaleVal})`;
+      scaleVal += speed;
+      window.requestAnimationFrame(loop);
+    }
+
+  $("#choose-day").submit((e) => {
+    e.preventDefault(); //prevents the form from returning to its initial state after the page reloads from triggering the submit btn.
+
+    // transition to different pulse states based on the day of the week.
+    let dayValue = $("#week-day").val();
+    if (dayValue === "Friday") {
+      transtate = "state-two";
+      circleScale = 0.8;
+      speed = 0.08;
+      if (scaleVal > 0.8) {
+        scaleVal = 0.8;
+      }
+    }
+    if (dayValue === "Saturday") {
+      transtate = "state-three";
+      circleScale = 1.5;
+      speed = 0.02;
+      if (scaleVal > 1.5) {
+        scaleVal = 1.5;
+      }
+    }
+    if (dayValue === "Sunday") {
+      transtate = "state-four";
+      circleScale = 1.1;
+      speed = 0.02;
+      if (scaleVal > 1.2) {
+        scaleVal = 1.2;
+      }
+    }
+    if (dayValue === "Monday") {
+      transtate = "state-five";
+      circleScale = 1;
+      speed = 0.05;
+      if (scaleVal > 1) {
+        scaleVal = 1;
+      }
+    }
+
+    $(`#results-container`).empty(); //resets the results every load.
+
+    // get the json data.
+    $.getJSON("loadFiles/stress.json", function (data) {
+      // set boolean to true
+      loaded = true;
+      findDayData(data, dayValue);
+    })
+      // fail
+      .fail(function () {
+        console.log("error");
+      });
+  });
+
+  //   Dsiplay the text results on screen.
+  function displayDay(day, dayQuality, sleepQuality) {
+    let textDay = $(
+      `<h2>On ${day}, I had a ${dayQuality} day.</h2> <h2>I got ${sleepQuality} hours of sleep.</h2>`
+    );
+    textDay.addClass("text-day").appendTo("#results-container");
+  }
+
+  // Does what the filter function does.
+  function findDayData(data, dayValue) {
+    for (let i = 0; i < data.days.length; i++) {
+      $.each(data.days[i], function (index, value) {
+        if (value === dayValue) {
+          displayDay(
+            data.days[i].day,
+            data.days[i].quality,
+            data.days[i].sleep
+          );
+        }
+      });
+    }
+  }
+
+  // // JQUERY ANIMATE
+  // $( "#submit-button" ).click(function() {
+  //   $( "#pulse" ).animate({
+  //     opacity: 0.5,
+  //     transform: "toggle"
+  //   }, 5000, function() {
+  //     // Animation complete.
+  //   });
+  // });
+
+  // https://www.kirupa.com/html5/looping_a_css_transition.htm
+  //   1. TRIGGER THE PULSE ANIMATION
+  let theCircle = document.querySelector("#pulse");
+
+  function setup() {
+    //   console.log( document.getElementById("submit-button"));
+    //  document.getElementById("submit-button").addEventListener("click", setInitialClass, false);
+    theCircle.addEventListener("mouseover", setInitialClass, false);
+
+    // theCircle.addEventListener("transitionend", loopTransition, false);
+    // theCircle.addEventListener("webkitTransitionEnd", loopTransition, false);
+    // theCircle.addEventListener("mozTransitionEnd", loopTransition, false);
+    // theCircle.addEventListener("msTransitionEnd", loopTransition, false);
+    // theCircle.addEventListener("oTransitionEnd", loopTransition, false);
+  }
+  setup();
+
+  function setInitialClass(e) {
+    // theCircle.className = transtate;
+    // console.log(theCircle.className);
+  }
+
+  // function loopTransition(e) {
+  //   // alternate b/w classes
+  //   // console.log(transtate);
+  //   if (e.propertyName == "opacity") {
+  //     if (theCircle.className == transtate) {
+  //       theCircle.className = "state-one";
+  //     } else {
+  //       theCircle.className = transtate;
+  //     }
+  //   }
+  // }
+});
